1964c104bc49c4b7dd22beb2dacdc7ace9120a28
SaidMANAM <said.manam21@gmail.com>
2021-01-31 04:32:48 +0100

    Creating positive SKIP GRAMS

---
+++ WORD2VEC_SKIP_GRAM.py
@@ -0,0 +1,50 @@
+import io
+import itertools
+import numpy as np
+import os
+import re
+import string
+import tensorflow as tf
+import tqdm
+
+from tensorflow.keras import Model, Sequential
+from tensorflow.keras.layers import Activation, Dense, Dot, Embedding, Flatten, GlobalAveragePooling1D, Reshape
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
+SEED = 2
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+########################################################SKIP GRAM#############################
+sentence = "The wide road shimmered in the hot sun"
+tokens = list(sentence.lower().split())
+print(tokens)
+
+vocab, index = {}, 1  # start indexing from 1
+vocab['<pad>'] = 0  # add a padding token
+for token in tokens:
+    if token not in vocab:
+        vocab[token] = index
+        index += 1
+vocab_size = len(vocab)
+#print(vocab)
+
+inverse_vocab = {index: token for token, index in vocab.items()}
+#print(inverse_vocab)
+
+example_sequence = [vocab[word] for word in tokens]
+print(example_sequence)
+
+window_size = 2
+positive_skip_grams, _ = tf.keras.preprocessing.sequence.skipgrams(
+    example_sequence,
+    vocabulary_size=vocab_size,
+    window_size=window_size,
+    negative_samples=0)
+for target, context in positive_skip_grams[:1]:
+    print(f"({target}, {context}): ({inverse_vocab[target]}, {inverse_vocab[context]})")
+
+########################################################NEGATIVE SAMPLING#############################
+
+# Get target and context words for one positive skip-gram.
+target_word, context_word = positive_skip_grams[0]
+print("Target:{}, Context:{}".format(target_word,context_word))
+print("Target:{}, Context:{}".format(inverse_vocab[target_word],inverse_vocab[context_word]))
